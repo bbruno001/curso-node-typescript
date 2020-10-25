7f6e486f8ec8f5aa77f391a29cc0a9071d60649f
bbruno001 <bruno_developer@outlook.com>
2020-10-24 23:44:54 -0300

    cap2-aula1-2

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,7 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours';
-import stormGlassNomalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours'
+import stormGlassWeather3HoursFixture from '../../../test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNomalized3HoursFixture from '../../../test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -10,11 +10,11 @@ describe('StormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue({stormGlassWeather3HoursFixture});
+        axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3HoursFixture});
 
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
-        
-        expect(response).toEqual({stormGlassNomalized3HoursFixture});
+
+        expect(response).toEqual(stormGlassNomalized3HoursFixture);
     });
 });
