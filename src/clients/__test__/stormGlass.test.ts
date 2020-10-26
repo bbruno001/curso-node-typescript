636ea7f17595620a7ccc2726574cffe8984034cb
bbruno001 <bruno_developer@outlook.com>
2020-10-26 20:26:24 -0300

    cap2-aula1-2

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,11 +6,13 @@ import stormGlassNomalized3HoursFixture from '../../../test/fixtures/stormglass_
 jest.mock('axios');
 
 describe('StormGlass client', () => {
-    it('should return the normalized forecast from the StormGlass service', async() => {
+    it('should return the normalized forecast from the StormGlass service', async () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3HoursFixture});
+        axios.get = jest
+            .fn()
+            .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
